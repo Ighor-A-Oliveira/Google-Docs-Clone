235b0de8ebcb7027bb526f5211592b1f1e85c6b5
Ighor Oliveira <ighor9991@gmail.com>
2025-01-30 14:58:24 -0300

    chaging things

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7PFMWtknOvU7_lWvj5FOgeeI-M_n4bAo",
-  authDomain: "docs-clone-488f2.firebaseapp.com",
-  projectId: "docs-clone-488f2",
-  storageBucket: "docs-clone-488f2.firebasestorage.app",
-  messagingSenderId: "998590469822",
-  appId: "1:998590469822:web:a791a4cdddeb196c0570ef"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
